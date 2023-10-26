5caf5c08ce46dffac8d5afc9b4fc38bcfe97d812
Helal Anwar <hilal123anwar@gmail.com>
2023-10-26 12:13:58 +0530

    Initial Commit

--- src/main/java/org/graded/classes/game/TicTacToe.java
+++ src/main/java/org/graded/classes/game/TicTacToe.java
@@ -30,20 +30,31 @@ public class TicTacToe {
             System.out.println("Enter the row and column");
             row = in.nextInt() - 1;
             column = in.nextInt() - 1;
-            System.out.println("\033[H\033[J");
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (i == row && j == column)
-                        box = box.replace(p[i][j], sign);
-                    else box = box.replace(p[i][j], " ");
+            System.out.println(row + "   " + column);
+            if (checkValidOrNot(row, column)) {
+                System.out.println("Invalid row or column please put the correct " +
+                        "\nrange that is 1 to 3 in both row  column");
+                k--;
+            } else {
+                System.out.println("\033[H\033[J");
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (i == row && j == column)
+                            box = box.replace(p[i][j], sign);
+                        else box = box.replace(p[i][j], " ");
 
+                    }
+                    System.out.println();
                 }
-                System.out.println();
+                box_in_memory = box_in_memory.replace(p[row][column], sign);
+                sign = sign.equals("X") ? "O" : "X";
+                System.out.println(box);
+                box = box_in_memory;
             }
-            box_in_memory = box_in_memory.replace(p[row][column], sign);
-            sign = sign.equals("X") ? "O" : "X";
-            System.out.println(box);
-            box = box_in_memory;
         }
+    }
+
+    private static boolean checkValidOrNot(int row, int column) {
+        return (row > 3 ||row < 0 || column > 3 || column < 0);
     }
 }
