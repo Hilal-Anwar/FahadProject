9538ca0dedf03f90c28d14e6022d96827637700f
Hilal-Anwar <hilal123anwar@gmail.com>
2024-07-27 15:17:00 +0530

    added new question

---
+++ src/main/java/loops/Output1.java
@@ -0,0 +1,19 @@
+package loops;
+
+/*
+Give the Output of the following
+ */
+public class Output1 {
+    public static void main(String[] args) {
+      /*
+      - for loop (entry control loop)
+      - while (entry control loop)
+      - do while (exit  control loop)
+
+       */
+
+        // This is a comment
+
+
+    }
+}
--- src/main/java/string_question/StringBasic.java
+++ src/main/java/string_question/StringBasic.java
@@ -2,38 +2,19 @@ package string_question;
 
 public class StringBasic {
     public static void main(String[] args) {
-        String s = " Yusuf Aziz ";
-        String s1 = "Apple";
-        int l = s.length();
-        char c = s.charAt(1);
-        int a = s.indexOf('s');
-        int b = s.indexOf('u');
-        int d = s.lastIndexOf('u');
-        String e = s.trim();
-        String f = s.toUpperCase();
-        String g = s.toLowerCase();
-        String h = s1.substring(1, 3);
-        String i = s1.substring(2);
-        System.out.println(l);
-        System.out.println(c);
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(h);
-        System.out.println(i);
-        String x = "Fatima";
-        String y = "Zahra";
-        System.out.println(x + " " + y);
-        System.out.println(x.concat(" ").concat(y));
-        String str = "hElal";
-        String str1 = "helal";
-        System.out.println(str.compareTo(str1));
-        String m="Motashabehat";
-        String n="Helal";
-        System.out.println(m.compareTo(n));
-
+        String s=" Java and bluej ";
+        /*
+        s=J a v a  a n d  b l u  e  j
+          0 1 2 3 4 5 6 7 8 9 10 11 12
+         */
+        System.out.println(s.length());
+        System.out.println(s.charAt(13));
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.lastIndexOf('a'));
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.trim());
+        System.out.println(s.substring(1,5));
 
     }
 }
