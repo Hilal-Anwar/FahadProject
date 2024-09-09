afbaf2113c28b7b2dfe01038d14c9d297ad2f6f1
Hilal-Anwar <hilal123anwar@gmail.com>
2024-09-09 11:52:45 +0530

    added new question

--- src/main/java/rahmat/HelloYellow.java
+++ src/main/java/rahmat/HelloYellow.java
@@ -3,6 +3,7 @@ package rahmat;
 public class HelloYellow {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Hellow Yellow 😂😂😂😂");
+        System.out.print("Welcome to Hello Yellow 😂😂😂😂");
+        System.out.println("My name is Faraz and I work at hello yellow");
     }
 }
---
+++ src/main/java/rahmat/MemoryStructure.java
@@ -0,0 +1,14 @@
+package rahmat;
+
+public class MemoryStructure
+{
+    public static void main(String[] args)
+    {
+        int fra = 34;
+        double b = 5.87;
+        char c='h';
+        String name="Rahmat";
+        System.out.println(fra);
+        System.out.println(name);
+    }
+}
